e0491c062bc749ab4e9d3009010fc68d97cf6ac2
Endlosforscher <luigi.sabbetti@gmail.com>
2025-04-23 12:28:29 +0200

    fix build errors

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react'
+import { useState, useMemo } from 'react'
 import RecipeListing from '../components/RecipeListing'
 import useRecipes from '../hooks/useRecipes'
 import Spinner from '../components/Spinner'
